14eff1cb03ccb5c89ef10e37dbb1d8aa42e86a59
imranH <imd572258@gmail.com>
2022-02-17 21:11:57 +0600

    done

--- bootstrap/JS/app.js
+++ bootstrap/JS/app.js
@@ -1,25 +1,14 @@
 function incomeExpenses(allInput){
 
-     // income filed value..
-     const monthlyIncomeExpenses = document.getElementById(allInput);
-     const monthlyIncomeExpensesText = monthlyIncomeExpenses.value ;
-     const monthlyIncomeExpensesAmount = parseFloat(monthlyIncomeExpensesText);
-     return monthlyIncomeExpensesAmount;
+     // income filed value.
+
+        const monthlyIncomeExpenses = document.getElementById(allInput);
+        const monthlyIncomeExpensesText = monthlyIncomeExpenses.value ;
+        const monthlyIncomeExpensesAmount = parseFloat(monthlyIncomeExpensesText);
+
+        return monthlyIncomeExpensesAmount ;
+          
 };
-
-/* function balanceAmount(){
-
-     // show in ui total Expanse
-     const expanseBalance = document.getElementById('totalExpenses');
-     const expanseBalanceText = expanseBalance.innerText;
-     const expanseBalanceAmount = parseFloat(expanseBalanceText);
-      expanseBalance.innerText = totalExpanse ;
-
-
-
-}
- */
-
 /* income calculate */
 
 document.getElementById('calculate').addEventListener('click', function(){
@@ -29,57 +18,42 @@ document.getElementById('calculate').addEventListener('click', function(){
         const monthlyFoodCostAmount = incomeExpenses('foodInput');
             // rent cost amount
         const monthlyRentCostAmount = incomeExpenses('rentInput'); 
-
         const clothCostAmount = incomeExpenses('clothesInput');
-        // total Total Expenses: 
-        const totalExpanse = monthlyFoodCostAmount + monthlyRentCostAmount + clothCostAmount ;  
-
-        
-        // show in ui total Expanse
+    
+              // total Total Expenses: 
+         const totalExpanse = monthlyFoodCostAmount + monthlyRentCostAmount + clothCostAmount ; 
+            // show in ui total Expanse
         const expanseBalance = document.getElementById('totalExpenses');
         const expanseBalanceText = expanseBalance.innerText;
         const expanseBalanceAmount = parseFloat(expanseBalanceText);
-         expanseBalance.innerText = totalExpanse ;
-
- 
-        //  Balance 
+        expanseBalance.innerText = totalExpanse ;
+    
+             //  Balance
         const afterExpanseBalance =  monthlyIncomeAmount - totalExpanse;
-      
         //after Expanse Total Balance
         const totalBalance = document.getElementById('afterExpensesBalance');
         const totalBalanceText = totalBalance.innerText;
         const totalBalanceAmount = parseFloat(totalBalanceText);
         totalBalance.innerText = afterExpanseBalance ;
-
-})
-
-function PercentageCalculate(){
+        
+    });
 
 
-
-
-}
 
 // save money calculate
 
 document.getElementById('saveButton').addEventListener('click', function(){
-/*         const monthlyIncome = document.getElementById('incomeInput');
-        const monthlyIncomeText = monthlyIncome.value ;
-        const monthlyIncomeAmount = parseFloat(monthlyIncomeText);
-
- */
+       
         const monthlyIncomeAmount = incomeExpenses('incomeInput');
 
-
-      //  const monthlyIncomeAmount = PercentageCalculate()
         //Percentage calculate
         const saveMoney = document.getElementById('saveInput');
         const saveMoneyText = saveMoney.value ;
         const saveMoneyAmount = parseFloat(saveMoneyText); 
+
         const totalSaveMoney =  saveMoney.value /  100 ;
         const PercentageSveMoney = totalSaveMoney * monthlyIncomeAmount ;
  
-
             //save amount calculate
 
         const savingMoney = document.getElementById('savingAmount');
@@ -93,8 +67,7 @@ document.getElementById('saveButton').addEventListener('click', function(){
         const totalBalanceAmount = parseFloat(totalBalanceText);
         totalBalance.innerText = totalBalanceAmount ;
 
-        
-            
+    
         //remaining balance 
         const remainingBalance = document.getElementById('remainingBalance')  
          const remainingBalanceText = remainingBalance.innerText;
