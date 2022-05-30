d9a433ab2820d289e71a3d95068e0480f965008a
ImranHossain5722 <imd572258@gmail.com>
2022-05-31 01:15:24 +0600

    git chnage repo

--- bootstrap/JS/app.js
+++ bootstrap/JS/app.js
@@ -9,6 +9,8 @@ function incomeExpenses(allInput){
         return monthlyIncomeExpensesAmount ;
           
 };
+
+
 /* income calculate */
 
 document.getElementById('calculate').addEventListener('click', function(){
@@ -78,5 +80,97 @@ document.getElementById('saveButton').addEventListener('click', function(){
 
 
 })
+
+
+
+
+/*
+// income calculate
+
+document.getElementById('calculate').addEventListener('click', function(){
+    // income filed value..
+const monthlyIncome = document.getElementById('incomeInput');
+const monthlyIncomeText = monthlyIncome.value ;
+const monthlyIncomeAmount = parseFloat(monthlyIncomeText);
+
+    // food cost input value
+const  monthlyFoodCost = document.getElementById('foodInput');
+const monthlyFoodTextCost = monthlyFoodCost.value;
+const monthlyFoodCostAmount = parseFloat(monthlyFoodTextCost);
+
+    // rent cost input value
+const monthlyRentCost = document.getElementById('rentInput');
+const monthlyRentTextCost = monthlyRentCost.value ;
+const monthlyRentCostAmount = parseFloat(monthlyRentTextCost);
+
+    // cloth cost input filed 
+const clothCost = document.getElementById('clothesInput');
+const clothTextCost = clothCost.value ;
+const clothCostAmount = parseFloat(clothTextCost);
+
+// total Total Expenses: 
+const totalExpanse = monthlyFoodCostAmount + monthlyRentCostAmount + clothCostAmount ;  
+// show in ui total Expanse
+const expanseBalance = document.getElementById('totalExpenses');
+const expanseBalanceText = expanseBalance.innerText;
+const expanseBalanceAmount = parseFloat(expanseBalanceText);
+expanseBalance.innerText = totalExpanse ;
+
+
+//  Balance 
+const afterExpanseBalance =  monthlyIncomeAmount - totalExpanse;
+// console.log(afterExpanseBalance);
+
+//after Expanse Total Balance
+const totalBalance = document.getElementById('afterExpensesBalance');
+const totalBalanceText = totalBalance.innerText;
+const totalBalanceAmount = parseFloat(totalBalanceText);
+totalBalance.innerText = afterExpanseBalance ;
+
+})
+
+// save money calculate
+
+document.getElementById('saveButton').addEventListener('click', function(){
+    const monthlyIncome = document.getElementById('incomeInput');
+    const monthlyIncomeText = monthlyIncome.value ;
+    const monthlyIncomeAmount = parseFloat(monthlyIncomeText);
+
+    //Percentage calculate
+    const saveMoney = document.getElementById('saveInput');
+    const saveMoneyText = saveMoney.value ;
+    const saveMoneyAmount = parseFloat(saveMoneyText); 
+
+    const totalSaveMoney =  saveMoney.value /  100 ;
+    const PercentageSveMoney = totalSaveMoney * monthlyIncomeAmount ;
+
+
+        //save amount calculate
+
+    const savingMoney = document.getElementById('savingAmount');
+    const savingMoneyText = savingMoney.innerText;
+    const savingMoneyAmount = parseFloat(savingMoneyText);
+        savingMoney.innerText =PercentageSveMoney;
+
+        //aTotal Balance
+    const totalBalance = document.getElementById('afterExpensesBalance');
+    const totalBalanceText = totalBalance.innerText;
+    const totalBalanceAmount = parseFloat(totalBalanceText);
+    totalBalance.innerText = totalBalanceAmount ;
+
+    
+        
+    //remaining balance 
+    const remainingBalance = document.getElementById('remainingBalance')  
+     const remainingBalanceText = remainingBalance.innerText;
+     const remainingBalanceAmount = parseFloat(remainingBalanceText);
+     const remainingAmount =  totalBalanceAmount - PercentageSveMoney;
+     remainingBalance.innerText = remainingAmount ;
+
+
+
+})
+
+*/
 
 
